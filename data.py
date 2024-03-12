891e8b0ad2bc65add7ae9ba23941e7d0ff3476b9
Rahul Dev E <rahul.e.dev@gmail.com>
2024-03-12 14:39:59 -0400

    update code for random paired samples

--- data.py
+++ data.py
@@ -1,5 +1,61 @@
-from torch.utils.data import Dataset
 import random
+from collections import defaultdict
+
+import datasets as hds
+import pandas as pd
+from astartes import train_val_test_split
+from torch.utils.data import Dataset
+
+from utils import parallelize, standardize
+
+
+@parallelize(8)
+def standardize_df(df):
+    df["inchi"] = df["smiles"].map(standardize)
+    return df
+
+
+def tokenize(entry, tokenizer):
+    entry = dict(entry)
+    smiles = entry.pop("smiles")
+    encoded = tokenizer(
+        smiles,
+        truncation=False,
+        return_attention_mask=True,
+        return_special_tokens_mask=True,
+    )
+
+    return encoded
+
+
+def load_kasa_regression():
+    df = pd.read_csv("./KasA_SMM_regression.csv")
+    df = df[["SMILES", "Average Average Z Score"]]
+    df = df.rename({"SMILES": "smiles", "Average Average Z Score": "score"}, axis=1)
+
+    # df = standardize_df(df)
+
+    splits = train_val_test_split(
+        X=df["smiles"].to_numpy(),
+        y=df["score"].to_numpy(),
+        sampler="scaffold",
+        random_state=42,
+        return_indices=True,
+    )
+
+    train_ids, val_ids, test_ids = splits[-3], splits[-2], splits[-1]
+    df_train = df.iloc[train_ids].reset_index(drop=True)
+    df_val = df.iloc[val_ids].reset_index(drop=True)
+    df_test = df.iloc[test_ids].reset_index(drop=True)
+
+    return hds.DatasetDict(
+        {
+            "train": hds.Dataset.from_pandas(df_train),
+            "val": hds.Dataset.from_pandas(df_val),
+            "test": hds.Dataset.from_pandas(df_test),
+        }
+    )
+
 
 def pair_collate(batch, base_collator):
     batch_from = [item["from"] for item in batch]
--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import AutoConfig, AutoModel
+from transformers import AutoModel
+
 
 class DeltaModel(nn.Module):
     def __init__(self, base_transformer: AutoModel) -> None:
@@ -10,7 +11,7 @@ class DeltaModel(nn.Module):
 
         hidden_size = base_transformer.config.hidden_size
         self.delta_head = nn.Sequential(
-            nn.Linear(2*hidden_size, hidden_size),
+            nn.Linear(2 * hidden_size, hidden_size),
             nn.PReLU(),
             nn.Linear(hidden_size, 128),
             nn.PReLU(),
@@ -18,20 +19,20 @@ class DeltaModel(nn.Module):
             nn.PReLU(),
             nn.Linear(64, 32),
             nn.PReLU(),
-            nn.Linear(32, 1)
+            nn.Linear(32, 1),
         )
 
     def embed_batch(self, batch):
-        batch = {k:v for k,v in batch.items() if k in ['input_ids', 'attention_mask']}
+        batch = {k: v for k, v in batch.items() if k in ["input_ids", "attention_mask"]}
         embeddings_chunk = self.embedder(**batch).last_hidden_state
         embeddings_chunk = embeddings_chunk.sum(dim=-2)
         # embeddings_chunk = nn.functional.normalize(embeddings_chunk)
         return embeddings_chunk
-    
+
     def get_delta(self, from_embedding, to_embedding):
         inp = torch.cat([from_embedding, to_embedding], axis=-1)
         return self.delta_head(inp)
-    
+
     def identity_loss(self, from_embeddings, to_embeddings):
         diff_a = self.get_delta(from_embeddings, from_embeddings)
         diff_b = self.get_delta(to_embeddings, to_embeddings)
@@ -39,7 +40,7 @@ class DeltaModel(nn.Module):
         # delta(a,a) ~ 0
         loss = diff_a.pow(2).sum().mean() + diff_b.pow(2).sum().mean()
         return loss
-    
+
     def inversion_loss(self, from_embeddings, to_embeddings):
         diff_a = self.get_delta(from_embeddings, to_embeddings)
         diff_b = self.get_delta(to_embeddings, from_embeddings)
@@ -49,14 +50,14 @@ class DeltaModel(nn.Module):
         return loss
 
     def forward(self, batch):
-        batch_from = batch['from']
-        batch_to = batch['to']
-        
+        batch_from = batch["from"]
+        batch_to = batch["to"]
+
         from_embeddings = self.embed_batch(batch_from)
         to_embeddings = self.embed_batch(batch_to)
 
         pred_delta = self.get_delta(from_embeddings, to_embeddings).squeeze()
-        actual_delta = batch_from['score'] - batch_to['score']
+        actual_delta = batch_from["score"] - batch_to["score"]
 
         # losses
         id_loss = self.identity_loss(from_embeddings, to_embeddings)
@@ -64,4 +65,11 @@ class DeltaModel(nn.Module):
 
         delta_loss = nn.functional.mse_loss(pred_delta, actual_delta)
         total_loss = delta_loss + id_loss + inv_loss
+        return {
+            "loss": total_loss,
+            "id_loss": id_loss,
+            "inv_loss": inv_loss,
+            "delta_loss": delta_loss,
+        }
+
         return {'loss': total_loss, 'id_loss': id_loss, 'inv_loss': inv_loss, 'delta_loss': delta_loss}
